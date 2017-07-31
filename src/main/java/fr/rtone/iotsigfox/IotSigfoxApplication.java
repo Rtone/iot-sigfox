354075cc4472976438dea5bc4b91be9954e7a82f
Hani <h.benzaouache@gmail.com>
2017-07-31 20:32:58 +0200

    Init Spring boot project

---
+++ src/main/java/fr/rtone/iotsigfox/IotSigfoxApplication.java
@@ -0,0 +1,12 @@
+package fr.rtone.iotsigfox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotSigfoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotSigfoxApplication.class, args);
+	}
+}
